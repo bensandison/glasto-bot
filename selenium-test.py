f4a8a922847e97762fbc35d863b2b7b1d745d09a
bensandison <sandisonben@gmail.com>
2022-11-01 16:17:51 +0000

    working page refresh with new IP's

--- selenium-test.py
+++ selenium-test.py
@@ -2,25 +2,31 @@ from selenium import webdriver
 import os
 from nordvpn_switcher import initialize_VPN, rotate_VPN
 
-
-instructions = initialize_VPN(area_input=['random countries europe 20'])
-
-for i in range(3):
-    rotate_VPN(instructions)  # refer to the instructions variable here
+# TODO: optimise selenium browser (no images)
+# Stop when reaches ticket page
+# Detect and move off slow connections
 
 URL = "https://glastonbury.seetickets.com/content/extras"
+isOnTicketPage = False
+refreshPerIP = 50
+refreshCounter = 0
 
-# Setup Chrome Driver
-PATH = os.environ.get('CHROMEDRIVER')
+# Setup VPN rotator:
+instructions = initialize_VPN(area_input=['random countries europe 9'])
+
+# Setup Chrome Driver:
+PATH = os.environ.get('CHROMEDRIVER')   # get driver location from env variable
 if not PATH:
     print("chrome driver env variable not found")
 driver = webdriver.Chrome(PATH)
 
-# Open URL in browser
-driver.get(URL)
 
-for x in range(50):
-    print(driver.title)
-    driver.refresh()
+while not isOnTicketPage:
+    driver.delete_all_cookies()  # delete cookies
+    rotate_VPN(instructions)   # Get new IP
 
-driver.close()
+    for x in range(refreshPerIP):
+        # Open URL in browser
+        driver.get(URL)  # refresh
+        refreshCounter += 1     # increment counter
+        print("Refresh " + str(refreshCounter))
