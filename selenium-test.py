69c21f8d78149867ffaf10a4a3c13fb4a682f3f3
bensandison <sandisonben@gmail.com>
2022-11-01 00:49:25 +0000

    rotate 20 countries

--- selenium-test.py
+++ selenium-test.py
@@ -3,12 +3,10 @@ import os
 from nordvpn_switcher import initialize_VPN, rotate_VPN
 
 
-instructions = initialize_VPN(area_input=['random countries europe 8'])
+instructions = initialize_VPN(area_input=['random countries europe 20'])
 
 for i in range(3):
     rotate_VPN(instructions)  # refer to the instructions variable here
-    print('\nDo whatever you want here (e.g.scraping). Pausing for 10 seconds...\n')
-
 
 URL = "https://glastonbury.seetickets.com/content/extras"
 
