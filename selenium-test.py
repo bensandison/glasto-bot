7c43a9443cf2868e55ba2476f83700acece21a2c
bensandison <sandisonben@gmail.com>
2022-11-05 20:34:46 +0000

    final bot

--- selenium-test.py
+++ selenium-test.py
@@ -4,13 +4,10 @@ import os
 from nordvpn_switcher import initialize_VPN, rotate_VPN
 import re
 
-# TODO:
-# Detect and move off slow connections
-
-URL = "https://glastonbury.seetickets.com/content/extras"
+URL = "https://glastonbury.seetickets.com/event/glastonbury-2023-deposits/worthy-farm/2500000"
 SEARCH_TERM = "worthy"
 isOnTicketPage = False
-refreshPerIP = 50
+refreshPerIP = 59
 refreshCounter = 0
 
 # Setup VPN rotator:
