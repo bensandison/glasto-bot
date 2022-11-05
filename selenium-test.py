ebec686787b6fb15e9dbaf8ffa10d76c1be07d75
bensandison <sandisonben@gmail.com>
2022-11-05 23:51:25 +0000

    new link

--- selenium-test.py
+++ selenium-test.py
@@ -4,7 +4,10 @@ import os
 from nordvpn_switcher import initialize_VPN, rotate_VPN
 import re
 
-URL = "https://glastonbury.seetickets.com/event/glastonbury-2023-deposits/worthy-farm/2500000"
+# url's:
+# URL = "https://glastonbury.seetickets.com/content/extras"
+URL = "https://glastonbury.seetickets.com/event/glastonbury-2023/worthy-farm/2500000"
+
 SEARCH_TERM = "worthy"
 isOnTicketPage = False
 refreshPerIP = 59
